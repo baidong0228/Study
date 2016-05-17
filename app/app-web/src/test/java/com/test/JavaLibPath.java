26903c1208d6f7e296075452f56929abb19838d4
Dwhite <baidong0228@foxmail.com>
2016-05-17 10:38:12 +0800

    sa

--- app/app-web/src/test/java/com/test/JavaLibPath.java
+++ app/app-web/src/test/java/com/test/JavaLibPath.java
@@ -6,12 +6,15 @@
  * and shall use it only in accordance with the terms of the agreements   
  * you entered into with Founder.   
  *   
- */     
-package com.test;    
-    
+ */
+package com.test;
+
+/**
+ * 打印java.library.path
+ */
 public class JavaLibPath {
     public static void main(String[] args) {
-    System.out.println(System.getProperty("java.library.path"));
+        System.out.println(System.getProperty("java.library.path"));
     }
 }
 
