2a1f2df64a4ba1c6a3b9f3e6cfe4fa76b59ba505
Dwhite <baidong0228@foxmail.com>
2016-05-25 23:22:23 +0800

    123213213

---
+++ app/app-base/app-base-common/src/main/java/com/bdong/JavaLibPath.java
@@ -0,0 +1,21 @@
+/*   
+ * Copyright (c) 2016-2026 Founder Bdong. All Rights Reserved.   
+ *   
+ * This software is the confidential and proprietary information of   
+ * Founder. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with Founder.   
+ *   
+ */
+package com.bdong;
+
+/**
+ * 打印java.library.path
+ */
+public class JavaLibPath {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
+
+  
